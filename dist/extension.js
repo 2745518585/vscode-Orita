47b157d9e1420c17e9f6d40d2f56692c0407deb6
2745518585 <2745518585@qq.com>
2023-02-16 13:03:17 +0800

    v0.0.1

--- dist/extension.js
+++ dist/extension.js
@@ -46,16 +46,12 @@ const vscode = __webpack_require__(1);
 function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('orita.compile-run', function () {
         const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor) {
-            vscode.window.showErrorMessage('Invalid File');
+        if (!activeEditor)
             return;
-        }
         activeEditor.document.save();
         const file = activeEditor.document.fileName;
-        if (file.substring(file.length - 4, file.length) != ".cpp") {
-            vscode.window.showErrorMessage('Invalid File');
+        if (file.substring(file.length - 4, file.length) != ".cpp")
             return;
-        }
         const file_address = file.substring(0, file.lastIndexOf('\\'));
         const file_name = file.substring(file.lastIndexOf('\\') + 1, file.length - 4);
         let Terminal = vscode.window.activeTerminal;
@@ -69,16 +65,12 @@ function activate(context) {
     }));
     context.subscriptions.push(vscode.commands.registerCommand('orita.compile', function () {
         const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor) {
-            vscode.window.showErrorMessage('Invalid File');
+        if (!activeEditor)
             return;
-        }
         activeEditor.document.save();
         const file = activeEditor.document.fileName;
-        if (file.substring(file.length - 4, file.length) != ".cpp") {
-            vscode.window.showErrorMessage('Invalid File');
+        if (file.substring(file.length - 4, file.length) != ".cpp")
             return;
-        }
         const file_address = file.substring(0, file.lastIndexOf('\\'));
         const file_name = file.substring(file.lastIndexOf('\\') + 1, file.length - 4);
         let Terminal = vscode.window.activeTerminal;
@@ -90,16 +82,11 @@ function activate(context) {
     }));
     context.subscriptions.push(vscode.commands.registerCommand('orita.run', function () {
         const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor) {
-            vscode.window.showErrorMessage('Invalid File');
+        if (!activeEditor)
             return;
-        }
-        activeEditor.document.save();
         const file = activeEditor.document.fileName;
-        if (file.substring(file.length - 4, file.length) != ".cpp") {
-            vscode.window.showErrorMessage('Invalid File');
+        if (file.substring(file.length - 4, file.length) != ".cpp")
             return;
-        }
         const file_address = file.substring(0, file.lastIndexOf('\\'));
         const file_name = file.substring(file.lastIndexOf('\\') + 1, file.length - 4);
         let Terminal = vscode.window.activeTerminal;
@@ -108,14 +95,53 @@ function activate(context) {
         }
         Terminal.show();
         Terminal.sendText('cd \"' + file_address + '\"');
-        Terminal.sendText('run /f \"' + file_name + '.cpp\"');
+        Terminal.sendText('.\\"' + file_name + '.exe\"');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('orita.inst-run', function () {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor)
+            return;
+        activeEditor.document.save();
+        const file = activeEditor.document.fileName;
+        if (file.substring(file.length - 4, file.length) != ".cpp")
+            return;
+        let Terminal = vscode.window.activeTerminal;
+        if (!Terminal) {
+            Terminal = vscode.window.createTerminal('powershell');
+        }
+        Terminal.show();
+        Terminal.sendText('run /f \"' + file + '\"');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('orita.inst-chdata-in', function () {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor)
+            return;
+        activeEditor.document.save();
+        const file = activeEditor.document.fileName;
+        let Terminal = vscode.window.activeTerminal;
+        if (!Terminal) {
+            Terminal = vscode.window.createTerminal('powershell');
+        }
+        Terminal.show();
+        Terminal.sendText('chdata /if \"' + file + '\"');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('orita.inst-chdata-out', function () {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor)
+            return;
+        activeEditor.document.save();
+        const file = activeEditor.document.fileName;
+        let Terminal = vscode.window.activeTerminal;
+        if (!Terminal) {
+            Terminal = vscode.window.createTerminal('powershell');
+        }
+        Terminal.show();
+        Terminal.sendText('chdata /of \"' + file + '\"');
     }));
     context.subscriptions.push(vscode.commands.registerCommand('orita.enter-address', function () {
         const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor) {
-            vscode.window.showErrorMessage('Invalid File');
+        if (!activeEditor)
             return;
-        }
         const file = activeEditor.document.fileName;
         const file_address = file.substring(0, file.lastIndexOf('\\'));
         let Terminal = vscode.window.activeTerminal;
--- src/extension.ts
+++ src/extension.ts
@@ -5,16 +5,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand('orita.compile-run', function () {
 		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) {
-			vscode.window.showErrorMessage('Invalid File');
-			return;
-		}
+		if (!activeEditor) return;
 		activeEditor.document.save();
 		const file = activeEditor.document.fileName;
-		if (file.substring(file.length - 4, file.length) != ".cpp") {
-			vscode.window.showErrorMessage('Invalid File');
-			return;
-		}
+		if (file.substring(file.length - 4, file.length) != ".cpp") return;
 		const file_address = file.substring(0, file.lastIndexOf('\\'));
 		const file_name = file.substring(file.lastIndexOf('\\') + 1, file.length - 4);
 		let Terminal = vscode.window.activeTerminal;
@@ -26,18 +20,13 @@ export function activate(context: vscode.ExtensionContext) {
 		Terminal.sendText('compile /f \"' + file_name + '.cpp\"');
 		Terminal.sendText('.\\"' + file_name + '.exe\"');
 	}));
+
 	context.subscriptions.push(vscode.commands.registerCommand('orita.compile', function () {
 		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) {
-			vscode.window.showErrorMessage('Invalid File');
-			return;
-		}
+		if (!activeEditor) return;
 		activeEditor.document.save();
 		const file = activeEditor.document.fileName;
-		if (file.substring(file.length - 4, file.length) != ".cpp") {
-			vscode.window.showErrorMessage('Invalid File');
-			return;
-		}
+		if (file.substring(file.length - 4, file.length) != ".cpp") return;
 		const file_address = file.substring(0, file.lastIndexOf('\\'));
 		const file_name = file.substring(file.lastIndexOf('\\') + 1, file.length - 4);
 		let Terminal = vscode.window.activeTerminal;
@@ -47,18 +36,12 @@ export function activate(context: vscode.ExtensionContext) {
 		Terminal.sendText('cd \"' + file_address + '\"');
 		Terminal.sendText('compile /f \"' + file_name + '.cpp\"');
 	}));
+
 	context.subscriptions.push(vscode.commands.registerCommand('orita.run', function () {
 		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) {
-			vscode.window.showErrorMessage('Invalid File');
-			return;
-		}
-		activeEditor.document.save();
+		if (!activeEditor) return;
 		const file = activeEditor.document.fileName;
-		if (file.substring(file.length - 4, file.length) != ".cpp") {
-			vscode.window.showErrorMessage('Invalid File');
-			return;
-		}
+		if (file.substring(file.length - 4, file.length) != ".cpp") return;
 		const file_address = file.substring(0, file.lastIndexOf('\\'));
 		const file_name = file.substring(file.lastIndexOf('\\') + 1, file.length - 4);
 		let Terminal = vscode.window.activeTerminal;
@@ -67,14 +50,52 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		Terminal.show();
 		Terminal.sendText('cd \"' + file_address + '\"');
-		Terminal.sendText('run /f \"' + file_name + '.cpp\"');
+		Terminal.sendText('.\\"' + file_name + '.exe\"');
 	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('orita.inst-run', function () {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) return;
+		activeEditor.document.save();
+		const file = activeEditor.document.fileName;
+		if (file.substring(file.length - 4, file.length) != ".cpp") return;
+		let Terminal = vscode.window.activeTerminal;
+		if (!Terminal) {
+			Terminal = vscode.window.createTerminal('powershell');
+		}
+		Terminal.show();
+		Terminal.sendText('run /f \"' + file + '\"');
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('orita.inst-chdata-in', function () {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) return;
+		activeEditor.document.save();
+		const file = activeEditor.document.fileName;
+		let Terminal = vscode.window.activeTerminal;
+		if (!Terminal) {
+			Terminal = vscode.window.createTerminal('powershell');
+		}
+		Terminal.show();
+		Terminal.sendText('chdata /if \"' + file + '\"');
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('orita.inst-chdata-out', function () {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) return;
+		activeEditor.document.save();
+		const file = activeEditor.document.fileName;
+		let Terminal = vscode.window.activeTerminal;
+		if (!Terminal) {
+			Terminal = vscode.window.createTerminal('powershell');
+		}
+		Terminal.show();
+		Terminal.sendText('chdata /of \"' + file + '\"');
+	}));
+
 	context.subscriptions.push(vscode.commands.registerCommand('orita.enter-address', function () {
 		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) {
-			vscode.window.showErrorMessage('Invalid File');
-			return;
-		}
+		if (!activeEditor) return;
 		const file = activeEditor.document.fileName;
 		const file_address = file.substring(0, file.lastIndexOf('\\'));
 		let Terminal = vscode.window.activeTerminal;
